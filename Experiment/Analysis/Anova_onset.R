6f0a0a6f280b802cbdeb0747758f2f178707b541
SeungchanGo <tmdcks6103@naver.com>
2025-01-25 23:38:30 +0900

    .

--- Experiment/Analysis/Anova_onset.R
+++
@@ -1,6 +0,0 @@
-setwd("C:/Users/tmdck/Desktop/R/data_Exp/onset")
-data = read.csv("data_long.csv", header = T)
-data$sbj <- factor(data$sbj)
-
-model2 <- aov(rt ~ (dist*offset*wm) + Error(sbj/(dist*offset*wm)), data)
-eta_squared(model2)
